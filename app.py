28087bbf9dedf02ae415a18b86944226e81b1847
EwertonIzidoroSilva <ewerton.santos@bei.eng.br>
2025-06-25 16:31:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from supabase import create_client, Client
 
+# --- Configurações do Supabase ---
 SUPABASE_URL = "https://xhbqtceonstbacfcgidr.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoYnF0Y2VvbnN0YmFjZmNnaWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NjIyMjMsImV4cCI6MjA2NjQzODIyM30.mml3sQJQhCWp_bNYKk7Edff-fBo1PDuqG7SPjw9bNWg"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+# --- Página ---
 st.set_page_config(page_title="Consulta de Estoque", page_icon="🔍")
 st.markdown("<h1 style='text-align: center;'>🔍 Consulta de Item no Estoque</h1>", unsafe_allow_html=True)
 
-# Aceita ID maiúsculo ou minúsculo
+# --- Leitura do parâmetro da URL, aceitando "id" ou "ID"
 query_params = st.query_params
 id_param = query_params.get("ID", [None])[0] or query_params.get("id", [None])[0]
 
@@ -26,9 +28,12 @@ if id_param:
             st.markdown(f"**Nome do Produto**: {item.get('NOME') or 'Não informado'}")
             st.markdown(f"**Quantidade em Estoque**: {item.get('QTDE_ATUAL') or 'Não informado'}")
             st.markdown(f"**Posição**: {item.get('POSICAO') or 'Não informado'}")
+
         else:
             st.error("❌ Item não encontrado no banco de dados.")
+
     except Exception as e:
         st.error(f"Erro ao consultar o banco de dados: {e}")
+
 else:
     st.info("🔎 Informe um ID na URL para realizar a consulta.\n\nExemplo:\n\n`?id=21168304736`")
